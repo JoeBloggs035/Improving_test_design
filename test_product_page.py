645d2876b5256398e7e076cfd3c247d42f6acd1a
Joe <JoeBloggs035@gmail.com>
2025-11-27 00:05:06 +0300

    добавлено неявное ожидание allert-а с проверочным кодом, и две секунды явного ожидания перед выполнением проверок соответствия фактического и ожидаемого результатов(специально для firefox)

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,4 @@
-# pytest -vs --tb=line --browser_name=chrome --language=en test_product_page.py
+# pytest -vs --tb=line --browser_name=firefox --language=fr test_product_page.py
 import time
 
 from .pages.product_page import ProductPage
@@ -10,6 +10,6 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.open()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
-    # time.sleep(30)
+    time.sleep(2) # специально для firefox
     product_page.expected_result1()
     product_page.expected_result2()
